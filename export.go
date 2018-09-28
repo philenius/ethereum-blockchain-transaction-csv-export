3e2f4e4815dca992501b9b9eb293e05f1de99851
philenius <philippus-perez@web.de>
2018-09-29 00:55:38 +0200

    working implementation with CSV export

---
+++ export.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mgutz/logxi/v1"
+	"github.com/onrik/ethrpc"
+)
+
+func exportAsCSV(txChan chan *ethrpc.Transaction) {
+
+	now := time.Now().Format("2006-01-02-15-04-05")
+	f, err := os.Create(fmt.Sprintf("geth_tx_export_%s.csv", now))
+	if err != nil {
+		log.Fatal("failed to open output file", "err", err.Error())
+	}
+
+	w := bufio.NewWriter(f)
+
+	w.WriteString("tx_hash,tx_nonce,tx_block_hash,tx_block_number,tx_index,tx_from,tx_to,tx_value,tx_gas,tx_gas_price,tx_input,tx_timestamp\n")
+	w.Flush()
+
+	for tx := range txChan {
+		w.WriteString(
+			fmt.Sprintf(
+				"%s,%d,%s,%d,%d,%s,%s,%d,%d,%d,%s\n",
+				tx.Hash, tx.Nonce, tx.BlockHash, tx.BlockNumber, tx.TransactionIndex, tx.From, tx.To, tx.Value.Int64(), tx.Gas, tx.GasPrice.Int64(), tx.Input,
+			),
+		)
+		w.Flush()
+	}
+
+	log.Info("finished writing to file", "file", f.Name())
+	w.Flush()
+}
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sync"
+
+	"github.com/mgutz/logxi/v1"
+	"github.com/onrik/ethrpc"
+)
+
+func main() {
+
+	hostname := flag.String("host", "127.0.0.1", "The hostname / IP address of the Ethereum node.")
+	port := flag.Int("port", 8545, "The port number of the Ethereum node.")
+	startHeight := flag.Int("start", 0, "The height / number of the block where to start.")
+	blockCount := flag.Int("count", 1000, "The total amount of blocks to fetch.")
+	flag.Parse()
+
+	client := ethrpc.New(fmt.Sprintf("http://%s:%d", *hostname, *port))
+	version, err := client.Web3ClientVersion()
+	if err != nil {
+		log.Fatal("failed to connect to Ethereum node", "err", err.Error())
+	}
+	log.Info("connected to Ethereum node", "version", version)
+
+	blockHeightChan := make(chan int, 10000)
+	txHashChan := make(chan string, 10000)
+	txChan := make(chan *ethrpc.Transaction, 10000)
+	wt := sync.WaitGroup{}
+
+	// list all blocks to fetch
+	go func() {
+		endHeight := *startHeight + *blockCount
+		for i := *startHeight; i < endHeight; i++ {
+			blockHeightChan <- i
+		}
+		log.Info("finished listing block numbers")
+		close(blockHeightChan)
+		wt.Done()
+	}()
+	wt.Add(1)
+
+	// fetch all blocks
+	go func() {
+		for blockHeight := range blockHeightChan {
+			block, err := client.EthGetBlockByNumber(blockHeight, true)
+			if err != nil {
+				log.Error("failed to get block", "blockNumber", *blockCount, "err", err.Error())
+				continue
+			}
+			log.Debug("successfully got block", "blockNumber", block.Number)
+			for _, tx := range block.Transactions {
+				txHashChan <- tx.Hash
+			}
+		}
+		log.Info("finished fetching all blocks")
+		close(txHashChan)
+		wt.Done()
+	}()
+	wt.Add(1)
+
+	// fetch all transactions
+	go func() {
+		for txHash := range txHashChan {
+			transaction, err := client.EthGetTransactionByHash(txHash)
+			if err != nil {
+				log.Error("failed to get transaction", "txiHash", txHash)
+				continue
+			}
+			txChan <- transaction
+			log.Debug("successfully got transaction", "txHash", transaction.Hash)
+		}
+		close(txChan)
+		log.Info("finished fetching all transactions")
+		wt.Done()
+	}()
+	wt.Add(1)
+
+	go func() {
+		exportAsCSV(txChan)
+		wt.Done()
+	}()
+	wt.Add(1)
+
+	log.Info("waiting for application to fetch all data from Ethereum node")
+	wt.Wait()
+	log.Info("application completed")
+
+}
